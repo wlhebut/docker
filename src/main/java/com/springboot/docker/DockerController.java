d6b169506bbe937aea58854b60e5d9bcec9a64a5
wlhebut172 <584529403@qq.com>
2018-10-11 16:20:39 +0800

    docker test --jdk javaweb

---
+++ src/main/java/com/springboot/docker/DockerController.java
@@ -0,0 +1,13 @@
+package com.springboot.docker;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Docker!";
+    }
+}
